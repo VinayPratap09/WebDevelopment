29f1a0f489fa39dddbdcef482d047035baf4d434
VinayPratap09 <vinayprata1999@gmail.com>
2021-06-26 13:34:05 +0530

    insta autoLicker

--- Module1/Lec10_AsyncAwait/instalogin.js
+++ Module1/Lec10_AsyncAwait/instalogin.js
@@ -37,15 +37,27 @@ const search="pepCoding";
        await tab.waitForSelector('._9VEo1.T-jvg');
        await tab.click('._9VEo1.T-jvg');
        //open Post
-       await tab.waitForSelector('.v1Nh3.kIKUG._bz0w a[href="/p/CQP1UMJrjkk/"]');
-       await tab.click('.v1Nh3.kIKUG._bz0w a[href="/p/CQP1UMJrjkk/"]');
+       //await tab.waitForSelector('.v1Nh3.kIKUG._bz0w:first-child');
+       await tab.waitForSelector('.v1Nh3.kIKUG._bz0w:first-child');
+       await tab.click('.v1Nh3.kIKUG._bz0w:first-child');
+       // //like .QBdPU .FY9nT
+       //await navigationPromise;
+
        await tab.waitForSelector('.fr66n button.wpO6b .QBdPU  svg[aria-label="Like"]');
-        await tab.click('.fr66n button.wpO6b .QBdPU  svg[aria-label="Like"]');
-        //await tab.keyboard.press('Enter');
-        //right click for more posts
-        
-        await tab.waitForSelector('.coreSpriteRightPaginationArrow');
-        await tab.click('.coreSpriteRightPaginationArrow');
+       await tab.click('.fr66n button.wpO6b .QBdPU  svg[aria-label="Like"]');
+       await tab.keyboard.press('Enter');
+       //right click for more posts
+       // await navigationPromise;
+       await tab.waitForSelector('.coreSpriteRightPaginationArrow');
+       await tab.click('.coreSpriteRightPaginationArrow');
+       for(let i=0;i<10;i++)
+       {
+           await tab.waitForSelector('.fr66n button.wpO6b .QBdPU  svg[aria-label="Like"]');
+           await tab.click('.fr66n button.wpO6b .QBdPU  svg[aria-label="Like"]');
+           
+           await tab.waitForSelector('.coreSpriteRightPaginationArrow');
+            await tab.click('.coreSpriteRightPaginationArrow');
+       }
         console.log("Successfull");
     }
     catch(err){
